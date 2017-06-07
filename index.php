dbd46c51d1aba6a7311bcf9cd18d9c361f3bccc5
Grzegorz Adam Kowalski <grzegorz.adam.kowalski@outlook.com>
2017-06-07 18:42:33 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+defined('_JEXEC') or die;
+
+$app      = JFactory::getApplication();
+$user     = JFactory::getUser();
+$option   = $app->input->getCmd('option', '');
+$view     = $app->input->getCmd('view', '');
+$layout   = $app->input->getCmd('layout', '');
+$task     = $app->input->getCmd('task', '');
+$itemid   = $app->input->getCmd('Itemid', '');
+$sitename = $app->get('sitename');
+
+$this->setHtml5(true);
+
+$body_classes =
+  'site '
+  . $option
+	. ' view-' . $view
+	. ($layout ? ' layout-' . $layout : ' no-layout')
+	. ($task ? ' task-' . $task : ' no-task')
+	. ($itemid ? ' itemid-' . $itemid : '')
+  . ($this->direction === 'rtl' ? ' rtl' : '';
+
+?>
+<!DOCTYPE html>
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<jdoc:include type="head" />
+</head>
+<body class="<?php echo $body_classes; ?>">
+</body>
+</html>
