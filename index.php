88213b1ace60f08e2f51b0599fb11503b4925544
Grzegorz Adam Kowalski <grzegorz.adam.kowalski@outlook.com>
2018-07-20 16:47:09 +0200

    Poprawka marginesów dla custom modules

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ $this->setGenerator(null);
 $this->setHtml5(true);
 
 $doc->addStyleSheet($template_path . '/css/normalize.css');
-$doc->addStyleSheet($template_path . '/css/style.css?ver=201807201416');
+$doc->addStyleSheet($template_path . '/css/style.css?ver=201807201646');
 $doc->addStyleSheet($template_path . '/css/animate.min.css');
 
 $doc->addScript($template_path . '/js/countUp.js');
